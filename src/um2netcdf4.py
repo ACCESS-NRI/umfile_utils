Fix some indentation errors affecting Heaviside scaling

src/um2netcdf4.py
@@ -256,13 +256,13 @@ try:
         #remove singleton dim
         if len(sp) == 4 and sp[1] == 1:
             vval = vval[:,0,:,:]
-    # P LEV/UV GRID with missing values treated as zero;
-    # needs to be corrected by Heavyside fn
+        # P LEV/UV GRID with missing values treated as zero;
+        # needs to be corrected by Heavyside fn
         stash_section = vval.stash_section[0]
         stash_item = vval.stash_item[0]
         item_code = vval.stash_section[0]*1000 + vval.stash_item[0]
         if (30201 <= item_code <= 30303) and mask and item_code!=30301:
-            if type(heavyside)==type(None):
+            if not heavyside:
                 # set heavyside variable if doesn't exist
                 try:
                     heavyside = fi.variables['psag']
@@ -276,16 +276,16 @@ try:
                         heavyside=fi.variables['temp_1'] #second temp field is on pressure levels
                     except:
                         heavyside=fi.variables['temp'] #take temp if there is no temp_1
-                heavyside=np.array(heavyside[:]!=0,dtype=np.float32)
-                print(np.shape(heavyside))
-                # Mask variable by heavyside function
-                fVal = vval.getMissing()         	
-                if vval.shape == heavyside.shape:
-                    vval = MV.where(np.greater(heavyside,hcrit),vval/heavyside,fVal)
-                    vval.fill_value = vval.missing_value = fVal
-                else:
-                    print(vname,vval.shape,'!= heavyside',heavyside.shape)
-                    print(vname+' not masked')
+                    heavyside=np.array(heavyside[:]!=0,dtype=np.float32)
+                    # print(np.shape(heavyside))
+            # Mask variable by heavyside function
+            fVal = vval.getMissing()         	
+            if vval.shape == heavyside.shape:
+                vval = MV.where(np.greater(heavyside[:],hcrit),vval/heavyside[:],fVal)
+                vval.fill_value = vval.missing_value = fVal
+            else:
+                print(vname,vval.shape,'!= heavyside',heavyside.shape)
+                print(vname+' not masked')
         fo.variables[vname_out][:] = vval[:]
         print('written: ',varname, 'to',vname_out)
     print('finished')