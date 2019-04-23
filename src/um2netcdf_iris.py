Fix pressure level masking. Prevent creation of int64 dimension.

src/um2netcdf_iris.py
@@ -2,11 +2,29 @@ from __future__ import print_function
 import iris, numpy as np, datetime, sys, re
 import stashvar
 from iris.coords import CellMethod
-import cf_units
+import cf_units, cftime
 
 iris.FUTURE.netcdf_no_unlimited = True
 
-def cubewrite(cube,sman,compression):
+def convert_proleptic(time):
+    # Convert from hour to days and shift origin from 1970 to 0001
+    t0 = cftime.DatetimeProlepticGregorian(1,1,1)
+    # datetime gets handled like proleptic gregorian so simple difference works
+    # Need a copy because can't assign to time.points[i]
+    tvals = np.array(time.points)
+    tbnds = np.array(time.bounds)
+    for i in range(len(time.points)):
+        delta = time.units.num2date(tvals[i]) - t0
+        tvals[i] = delta.days + delta.seconds/86400.
+        delta = time.units.num2date(tbnds[i][0]) - t0
+        tbnds[i][0] = delta.days + delta.seconds/86400.
+        delta = time.units.num2date(tbnds[i][1]) - t0
+        tbnds[i][1] = delta.days + delta.seconds/86400.
+    time.points = tvals
+    time.bounds = tbnds
+    time.units = cf_units.Unit("days since 0001-01-01 00:00", calendar='proleptic_gregorian')
+
+def cubewrite(cube,sman,compression,use64bit):
     try:
         plevs = cube.coord('pressure')
         if plevs.points[0] < plevs.points[-1]:
@@ -19,21 +37,31 @@ def cubewrite(cube,sman,compression):
                 cube = cube[::-1]
     except iris.exceptions.CoordinateNotFoundError:
         pass
-    if cube.data.dtype == 'float64':
+    if cube.data.dtype == 'float64' and not use64bit:
         cube.data = cube.data.astype(np.float32)
 
     # If reference date is before 1600 use proleptic gregorian
-    # calendar and change units
+    # calendar and change units from hours to days
     time = cube.coord('time')
     reftime = cube.coord('forecast_reference_time')
     refdate = reftime.units.num2date(reftime.points[0])
-    if refdate.year < 1600 and time.units.calendar=='gregorian':
-        time.units = cf_units.Unit("days since 0001-01-01 00:00", calendar='proleptic_gregorian')
-        # Convert from hour to days and shift origin from 1970 to 0001
-        time.points = time.points/24. + 719164.
-        time.bounds = time.bounds/24. + 719164.
+    if time.units.calendar=='gregorian':
+        assert time.units.origin == 'hours since 1970-01-01 00:00:00'
+        if refdate.year < 1600:
+            convert_proleptic(time)
+        else:
+            time.units = cf_units.Unit("days since 1970-01-01 00:00", calendar='proleptic_gregorian')
+            time.points = time.points/24.
+            time.bounds = time.bounds/24.
         cube.remove_coord('forecast_period')
         cube.remove_coord('forecast_reference_time')
+
+    # Check whether any of the coordinates is a pseudo-dimension
+    # with integer values and if so reset to int32 to prevent
+    # problems with possible later conversion to netCDF3
+    for coord in cube.coords():
+        if coord.points.dtype == np.int64:
+            coord.points = coord.points.astype(np.int32)
 
     try:
         if cube.coord_dims('time'):
@@ -58,7 +86,7 @@ def fix_cell_methods(mtuple):
         newm.append(n)
     return tuple(newm)
 
-def process(infile,outfile,verbose=False,nckind=3,compression=4,nomask=False,include_list=None,exclude_list=None):
+def process(infile,outfile,verbose=False,nckind=3,compression=4,nomask=False,include_list=None,exclude_list=None,use64bit=False,nohist=False):
 
     if include_list and exclude_list:
         raise Exception("Error: include list and exclude list are mutually exclusive")
@@ -72,19 +100,30 @@ def process(infile,outfile,verbose=False,nckind=3,compression=4,nomask=False,inc
     # Check whether there are any pressure level fields that should be 
     # masked. Can use temperature to mask instantaneous fields, so really
     # should check whether these are time means
-    need_heaviside = False
-    have_heaviside = False
+    need_heaviside_uv = need_heaviside_t = False
+    have_heaviside_uv = have_heaviside_t = False
     hcrit = 0.5
     for c in cubes:
         stashcode = c.attributes['STASH']
-        if stashcode.section == 30 and 201 <= stashcode.item <= 303:
-            need_heaviside = True
-        if stashcode.section == 30 and stashcode.item == 301:
-            have_heaviside = True
-            heaviside = c
+        if ( stashcode.section == 30 and
+           ( 201 <= stashcode.item <= 288  or 302 <= stashcode.item <= 303 )):
 
-    if not nomask and need_heaviside and not have_heaviside:
-        print("""Warning - heaviside field needed for masking pressure level data is not present.
+            need_heaviside_uv = True
+        if stashcode.section == 30 and stashcode.item == 301:
+            have_heaviside_uv = True
+            heaviside_uv = c
+        if ( stashcode.section == 30 and 293 <= stashcode.item <= 298):
+
+            need_heaviside_t = True
+        if stashcode.section == 30 and stashcode.item == 304:
+            have_heaviside_t = True
+            heaviside_t = c
+
+    if not nomask and need_heaviside_uv and not have_heaviside_uv:
+        print("""Warning - heaviside_uv field needed for masking pressure level data is not present.
+    These fields will be skipped""")
+    if not nomask and need_heaviside_t and not have_heaviside_t:
+        print("""Warning - heaviside_t field needed for masking pressure level data is not present.
     These fields will be skipped""")
 
     nc_formats = {1: 'NETCDF3_CLASSIC', 2: 'NETCDF3_64BIT', 
@@ -92,8 +131,9 @@ def process(infile,outfile,verbose=False,nckind=3,compression=4,nomask=False,inc
     with iris.fileformats.netcdf.Saver(outfile, nc_formats[nckind]) as sman:
 
         # Add global attributes
-        history = "File %s with converted with um2netcdf_iris.py at %s" % (infile, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        sman.update_global_attributes({'history':history})
+        if not nohist:
+            history = "File %s with converted with um2netcdf_iris.py at %s" % (infile, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+            sman.update_global_attributes({'history':history})
         sman.update_global_attributes({'Conventions':'CF-1.6'})
 
         for c in cubes:
@@ -146,19 +186,30 @@ def process(infile,outfile,verbose=False,nckind=3,compression=4,nomask=False,inc
             # remove it.
             c.cell_methods = fix_cell_methods(c.cell_methods)
 
-            if stashcode.section == 30 and stashcode.item == 301:
+            if stashcode.section == 30 and stashcode.item in (301,304):
+                # Skip the mask fields themselves
                 continue
-            if not nomask and stashcode.section == 30 and 201 <= stashcode.item <= 303:
+            if not nomask and stashcode.section == 30 and \
+             (201 <= stashcode.item <= 288  or 302 <= stashcode.item <= 303):
                 # Pressure level data should be masked
-                if have_heaviside:
+                if have_heaviside_uv:
                     # Temporarily turn off warnings from 0/0
                     with np.errstate(divide='ignore',invalid='ignore'):               
-                        c.data = np.ma.masked_array(c.data/heaviside.data, heaviside.data <= hcrit).astype(np.float32)
+                        c.data = np.ma.masked_array(c.data/heaviside_uv.data, heaviside_uv.data <= hcrit).astype(np.float32)
+                else:
+                    continue
+            if not nomask and stashcode.section == 30 and \
+             (293 <= stashcode.item <= 298):
+                # Pressure level data should be masked
+                if have_heaviside_t:
+                    # Temporarily turn off warnings from 0/0
+                    with np.errstate(divide='ignore',invalid='ignore'):               
+                        c.data = np.ma.masked_array(c.data/heaviside_t.data, heaviside_t.data <= hcrit).astype(np.float32)
                 else:
                     continue
             if verbose:
                 print(c.name(), itemcode)
-            cubewrite(c,sman,compression)
+            cubewrite(c,sman,compression,use64bit)
 
 if __name__ == '__main__':
     import sys, argparse
@@ -167,6 +218,8 @@ if __name__ == '__main__':
                         default=3, help='specify kind of netCDF format for output file: 1 classic, 2 64-bit offset, 3 netCDF-4, 4 netCDF-4 classic model. Default 3', choices=[1,2,3,4])
     parser.add_argument('-c', dest='compression', required=False, type=int,
                         default=4, help='compression level (0=none, 9=max). Default 4')
+    parser.add_argument('--64', dest='use64bit', action='store_true', 
+                    default=False, help='Use 64 bit netcdf for 64 bit input')
     parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', 
                     default=False, help='verbose output')
     parser.add_argument('--include', dest='include', type=int,
@@ -175,9 +228,11 @@ if __name__ == '__main__':
                         nargs = '+', help = 'List of stash codes to exclude')
     parser.add_argument('--nomask', dest='nomask', action='store_true', 
                     default=False, help="Don't apply heaviside function mask to pressure level fields")
+    parser.add_argument('--nohist', dest='nohist', action='store_true', 
+                    default=False, help="Don't update history attribute")
     parser.add_argument('infile', nargs='?', help='Input file')
     parser.add_argument('outfile', nargs='?', help='Output file')
 
     args = parser.parse_args()
 
-    process(args.infile,args.outfile,args.verbose,args.kind,args.compression,args.nomask,args.include,args.exclude)
+    process(args.infile,args.outfile,args.verbose,args.kind,args.compression,args.nomask,args.include,args.exclude,args.use64bit,args.nohist)