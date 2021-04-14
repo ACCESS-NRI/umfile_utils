02ebc5eacddf2b37dfc91c81d32c4c978cf6ab86
MartinDix <martin.dix@csiro.au>
2021-04-14 11:26:44 +1000

    Add seed option to make it reproducible.

--- src/perturbIC.py
+++ src/perturbIC.py
@@ -6,35 +6,34 @@
 
 # Martin Dix martin.dix@csiro.au
 
-import numpy as np
-import getopt, sys
+import argparse, sys
 import umfile
 from um_fileheaders import *
+from numpy.random import MT19937, RandomState, SeedSequence
 
-amplitude = 0.01
-try:
-    optlist, args = getopt.getopt(sys.argv[1:], 'a:v:')
-    for opt in optlist:
-        if opt[0] == '-a':
-            amplitude = float(opt[1])
-        elif opt[0] == '-v':
-            stashcode = int(opt[1])
-except getopt.error:
-    print("Usage: perturbIC [-a amplitude] [-v variable (stashcode)] file")
-    sys.exit(2)
+parser = argparse.ArgumentParser(description="Perturb UM initial dump")
+parser.add_argument('-a', dest='amplitude', type=float, default=0.01,
+                    help = 'Amplitude of perturbation')
+parser.add_argument('-s', dest='seed', type=int, default=-1,
+    help = 'Random number seed (default -1 means non-reprodicible system initialisation)')
+parser.add_argument('ifile', help='Input file (modified in place)')
 
-ifile = args[0]
+args = parser.parse_args()
 
-f = umfile.UMFile(ifile, 'r+')
+if args.seed >= 0:
+    rs = RandomState(MT19937(SeedSequence(args.seed)))
+else:
+    # Default system initialisation (not reproducible)
+    rs = RandomState(MT19937(SeedSequence()))
+
+f = umfile.UMFile(args.ifile, 'r+')
 
 # Set up theta perturbation.
 nlon = f.inthead[IC_XLen]
 nlat = f.inthead[IC_YLen]
 # Same at each level so as not to upset vertical stability
-# Should really set the seed.
-perturb = amplitude * (2.*np.random.random(nlon*nlat).reshape((nlat,nlon)) - 1.)
-# Set poles to zero (only necessary for ND grids, but doesn't hurt
-# EG)
+perturb = args.amplitude * (2.*rs.random(nlon*nlat).reshape((nlat,nlon)) - 1.)
+# Set poles to zero (only necessary for ND grids, but doesn't hurt EG)
 perturb[0] = 0.
 perturb[-1] = 0.
 
