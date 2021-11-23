fd15a616df95827ea93ed9a6aeefed7d43f97d75
MartinDix <martin.dix@csiro.au>
2021-11-23 15:48:27 +1100

    Change default to zero all fields.

--- src/um_zero_field.py
+++ src/um_zero_field.py
@@ -4,35 +4,26 @@
 # Martin Dix martin.dix@csiro.au
 
 import numpy as np
-import getopt, sys
+import argparse, sys
 import umfile
 from um_fileheaders import *
 
-vlist = []
-try:
-    optlist, args = getopt.getopt(sys.argv[1:], 'v:')
-    for opt in optlist:
-        if opt[0] == '-v':
-            # Allow comma separated lists
-            for v in opt[1].split(","):
-                vlist.append(int(v))
-except getopt.error:
-    print "Usage: um_zero_field.py [-v var] file"
-    print "       -v var1,var2,... to zero only these variables"
-    print "       Variables specified by STASH index = Section Number * 1000 + item number"
-    sys.exit(2)
+parser = argparse.ArgumentParser(description="Set fields in UM file to zero")
+parser.add_argument('-v', dest='var', type=int, default=None,
+                    nargs = '+', help = 'List of stash codes to zero (default is all)')
+parser.add_argument('file', help='File to process (overwritten)')
 
-ifile = args[0]
+args = parser.parse_args()
 
-f = umfile.UMFile(ifile, 'r+')
+f = umfile.UMFile(args.file, 'r+')
 
 for k in range(f.fixhd[FH_LookupSize2]):
     ilookup = f.ilookup[k]
     lbegin = ilookup[LBEGIN] # lbegin is offset from start
     if lbegin == -99:
         break
-    if ilookup[ITEM_CODE] in vlist:
-        print "Zeroing", k, ilookup[ITEM_CODE]
+    if not args.var or ilookup[ITEM_CODE] in args.var:
+        print("Zeroing field", k, 'stash code', ilookup[ITEM_CODE])
         a = f.readfld(k)
         a[:] = 0.
         f.writefld(a,k)
