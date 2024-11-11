880b39371a84127226eecfca77ab803a75231649
Lindsey Oberhelman <120349369+leoberhelman@users.noreply.github.com>
2024-11-12 09:05:15 +1100

    Update perturbIC.py
    This implements a potential workaround for validation. It passes the validate attribute.

--- src/perturbIC.py
+++ src/perturbIC.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # Apply a perturbation to initial condition.
 # Note that this works in place.
 # For ENDGAME perturb thetavd as well if it's present
@@ -5,15 +7,13 @@
 # Martin Dix martin.dix@csiro.au
 
 import argparse
-import umfile
-from um_fileheaders import *
 from numpy.random import PCG64, Generator
 import mule
 
 
 def parse_args():
     """
-    This function parses the arguments from the command line.
+    This function parses the arguments from the command line
 
     Parameters
     ----------
@@ -22,7 +22,7 @@ def parse_args():
     Returns
     ----------
     args_parsed : ArguementParser object
-        Contains the arguments from the command line that can be access with the dest
+        Contains the arguments from the command line that can be access with their dest
     """
     parser = argparse.ArgumentParser(description="Perturb UM initial dump")
     parser.add_argument('-a', dest='amplitude', type=float, default=0.01,
@@ -32,47 +32,10 @@ def parse_args():
     parser.add_argument('-o', dest='output', type=str, default=None,
         help = 'Output file (if none given modified in place)')
     parser.add_argument('ifile', help='Input file (modified in place)')
+    parser.add_argument('-v', dest='validate',
+        help='For testing with ESM15 set True', default=True)
     args_parsed = parser.parse_args()
     return args_parsed
-
-def get_rid_of_timeseries(ff):
-    """
-    This function checks to see if there are times series, then gets rid of them so that 
-    mule can run.
-
-    Parameters
-    ----------
-    args : ArgumentParser object
-           The argument parser object with amplitude, seed from commandline
-
-    Returns
-    ----------
-    ff/ff_out : Returns a mule fields object with no timeseries
-    """
-    ff_out = ff.copy()
-    num_ts = 0
-
-    #Perform timeseries removal without rewriting file
-    for fld in ff.fields:
-
-        # Check for the grid code that denotes a timeseries
-        if fld.lbcode in (31320, 31323):
-            num_ts += 1
-
-        else:
-            ff_out.fields.append(fld)
-
-    #Either return the fields with the timeseries removed
-    if num_ts > 0:
-        print(f'{num_ts} timeseries fields skipped')
-        return ff_out
-    
-    #Or return all the feilds
-    else:
-        print('No timeseries fields found')
-        return ff
-    
-
 
 def set_seed(args):
     """
@@ -98,20 +61,60 @@ def set_seed(args):
     else:
         raise Exception('Seed must be positive')
 
+def get_rid_of_timeseries(ff):
+    """
+    This function checks to see if there are times series, then gets rid of them 
+    so that mule can run.
+
+    Parameters
+    ----------
+    args : ArgumentParser object
+           The argument parser object with amplitude, seed from commandline
+
+    Returns
+    ----------
+    ff/ff_out : Returns a mule fields object with no timeseries
+    """
+    ff_out = ff.copy()
+    num_ts = 0
+
+    # Perform timeseries removal without rewriting file
+    for fld in ff.fields:
+
+        # Check for the grid code that denotes a timeseries
+        if fld.lbcode in (31320, 31323):
+            num_ts += 1
+
+        else:
+            ff_out.fields.append(fld)
+
+    # Either return the fields with the timeseries removed
+    if num_ts > 0:
+        print(f'{num_ts} timeseries fields skipped')
+        return ff_out
+
+    # Or return all the feilds
+    else:
+        print('No timeseries fields found')
+        return ff
+
+
 def is_inplace(args):
     """
-    This function allows for editing out-of-place. 
+    This provides an outline for editing if a new file should be 
+    created
 
     Parameters
     ----------
     args: ArgumentParser object
-         The argument parser object with output file name.
+         The argument parser object with output file name
 
     Returns
     ----------
     Boolean - returns False for editing in place and True if a new 
         outfile has been created
 
+    subprocess.run(['cp', original_file, new_file])
     """
 
     if args.output == None:
@@ -123,22 +126,16 @@ def is_inplace(args):
 
 def create_perturbation(args, rs, nlon, nlat):
     """
-    This function create a random pertrbation of amplitude args.amplitude.
+    This function create a random pertrbation of amplitude args.amplitude 
 
     Parameters
     ----------
-    args : Dictionary
-        The argumenst from the commandline (amplitude, seed).
+    args : Dictionary - The argumenst from the commandline (amplitude, seed)
+        rs : Random Object - The random object that has a seed (if defined)
+           Argument 2 description
+    nlon: Int - This is the lon
+           Argument 3 description
 
-    rs : Random Object 
-        The random object that has a seed (if defined).
-           
-    nlon: Int 
-        This is the longitude limit for the grid.
-
-    nlat: Int
-        This is the latitude limit for the grid.
-       
     Returns
     ----------
     pertubation -  Array - Returns a perturbation where the poles are set to 0
@@ -151,103 +148,155 @@ def create_perturbation(args, rs, nlon, nlat):
     perturbation[-1] = 0
     return perturbation
 
-def is_end_of_file(field,data_limit):
+def is_end_of_file(field_data, data_limit):
     """
-    This function checks to see if there is data associated with the metadata.
+    This function checks to see if there is data associated with the metadata
 
     Parameters
     ----------
     f : umFile Object 
-        This is the fields file that holds the restart data. 
+        This is the fields file that holds the restart data 
     
     k : int
-        This integer is indexing the metadata in the fields file.
+        This integer is indexing the metadata in the fields file
 
     data_limit : int
-        This int is a placeholder indicating the end of the data.
+        This int is a placeholder indicating the end of the data
     Returns
     ----------
-    boolean - True if the end of the data is reached and False everwhere else.
+    boolean - True if the end of the data is reached and False everwhere else
 
     """
 
-    if field.lbuser4 == data_limit:
+    if field_data  == data_limit:
         return True
     else:
         return False
 
-def do_perturb(field, surface_temp_code):
+def do_perturb(field, surface_stash_code):
     """
-    This function checks to make sure that the correct field is used (surface temperature).
+    This function checks to make sure that the correct field is used (surface temperature)
 
     Parameters
     ----------
     
     field : mule fields Object
-           Holds the entire umfile including metadata and data.
+           Holds the entire umfile including metadata and datai
 
     surface_stash_code : int
-        This integer represents the item code for surface temperature or theta.
-    
 
     Returns
     ----------
     boolean - True if this is the correct data to be perturbed. False for all other item code
 
     """
-         
-    if field.lbuser4 == surface_temp_code:
+    if field.lbuser4 == surface_stash_code:
         return True
 
     else:
         return False
 
+class SetPerturbOperator(mule.DataOperator):
+    """
+    This class creates a mule operator that adds a random perturbation to a field
+
+    Parameters
+    __________
+
+    perturb : np.array
+             An array of the random values to be added to the field
+    
+    Returns
+    __________
+
+    field - The field with the perturbation added to it
+    """
+    def __init__(self, perturb):
+        self.perturbation = perturb
+
+    def new_field(self, source_field):
+        """Creates the new field object"""
+        return source_field.copy()
+
+    def transform(self, source_field, new_field):
+        """Performs the data manipulation"""
+        data = source_field.get_data()
+
+        # Multiply by 0 to keep the array shape
+        return data + self.perturbation
+
+
+def void(*args, **kwargs):
+    print('skipping validation')
+    pass
+
+
+def set_validation(validate):
+    """
+    This function sets the validation. It is for testing purposes to cirucmvent the river fields flipped grid in ESM15
+
+    Parameters
+    __________
+
+    validate : boolean
+              This variable is mandatory from the user and is True for testing purposes
+
+    """
+    if validate:
+        mule.DumpFile.validate = void
+
+    else:
+        print("May encounter an error  if using ESM15 with the river field grids set validate to True to circumvent")
+
 def main():
     """
-    This function checks to make sure that the correct field is used (surface temperature).
-
-    Returns
-    ----------
-    UM fields file - The modifed fields file that has the perturbed field.
-
+    This function executes all the steps to add the perturbation.The results if saving the perturbation 
+    in the restart file. 
     """
 
     # Define all the variables  
     data_limit = -99
-    surface_temp_code = 24
+    surface_temp_stash = 4
 
     # Obtain the arguements from the commandline
-    # Then set the seed if there is one
+    # Set the seed if one is given else proced without one.
     args = parse_args()
     random_obj = set_seed(args)
 
-    #Check if there is timeseries then remove them to avoid mule errors.
-    ff_raw = mule.FieldsFile.from_file(args.ifile)
+    # Skips the validation entirely for use on ESM15 due to river fields error
+    set_validation(args.validate)
 
-    # The definitions of the grid
+    ff_raw = mule.DumpFile.from_file(args.ifile)
+
+    # Get the  definitions of the grid from the ff object
     # Set up theta perturbation.
-    nlon = ff.integer_constants.num_cols
-    nlat = ff.integer_constants.num_rows
+    nlon = ff_raw.integer_constants.num_cols
+    nlat = ff_raw.integer_constants.num_rows
 
     # Remove the time series from the data to ensure mule will work
     ff = get_rid_of_timeseries(ff_raw)
 
-    # Same at each level so as not to upset vertical stability.
+    # Create a random perturbation array and set it up as a mule operator
+    # Same at each level so as not to upset vertical stability
     perturbation = create_perturbation(args, random_obj, nlon, nlat)
+    setperturbation = SetPerturbOperator(perturbation)
 
+    #Loop through the fields to find the surface termperature
     for ifield, field in enumerate(ff.fields):
 
-        if is_end_of_file(field, data_limit):
+        if is_end_of_file(field.lbuser4, data_limit):
             break
-        # Find the surface temperature field and add the perturbation.
-        if do_perturb(field, surface_temp_code):
 
-            data = field.get_data()
-            ff.fields[ifield] = data+perturbation
+        # Find the surface temperature field and add the perturbation
+        if field.lbuser4 == surface_temp_stash:
+            ff.fields[ifield] = setperturbation(field)
 
+
+    #Not currently set up to do out-of-place yet
     ff.to_file(args.ifile)
-
 
 if __name__== "__main__":
 
     main()
+
+
