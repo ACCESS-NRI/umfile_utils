a6ba0940d379fa67f54553c121db7cba1e59b994
Lindsey Oberhelman <120349369+leoberhelman@users.noreply.github.com>
2024-11-20 15:17:43 +1100

    Apply suggestions from code review
    Co-authored-by: Davide Marchegiani <davide.marchegiani@gmail.com>

--- setup.py
+++ setup.py
@@ -1,14 +1,7 @@
-from setuptools import setup, find_packages
+import versioneer
+from setuptools import setup
 
 setup(
-    name="umfile_utils",
-    version="0.1.0",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    install_requires=[
-        # Add your dependencies here, e.g., "numpy", "requests", etc.
-    ],
-    entry_points={
-        # Optional: Add command line scripts or other entry points here.
-    },
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
 )
