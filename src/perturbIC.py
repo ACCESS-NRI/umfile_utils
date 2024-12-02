b86c6a4484d3907e6afe2db5539380539266194f
Lindsey Oberhelman <120349369+leoberhelman@users.noreply.github.com>
2024-12-02 19:40:42 +1100

    Update src/perturbIC.py
    Co-authored-by: Davide Marchegiani <davide.marchegiani@gmail.com>

--- src/perturbIC.py
+++ src/perturbIC.py
@@ -50,8 +50,6 @@ def create_random_generator(value=None):
     numpy.random.Generator
         The numpy random generator object.
     """
-
-
     if value < 0:
         raise ValueError('Seed value must be non-negative.')
     return Generator(PCG64(value))
