abca358eb58884a6867b24a1a733f3d98b077f4e
Lindsey Oberhelman <120349369+leoberhelman@users.noreply.github.com>
2025-01-28 10:15:17 +1100

    Update change_dump_date.py
    Converted to mule. Still unsure if changing the correct times

--- src/change_dump_date.py
+++ src/change_dump_date.py
@@ -6,42 +6,86 @@
 # xconv less confusing.
 
 import getopt, sys
-import umfile
-from um_fileheaders import *
+import argparse
+import mule
 
-ifile = sys.argv[1]
 
-f = umfile.UMFile(ifile, "r+")
+def parse_args():
+    """
+   Parse the command line arguments.
 
-print("Initial Time", f.fixhd[FH_DTYear], f.fixhd[FH_DTMonth],
-      f.fixhd[FH_DTDay], f.fixhd[FH_DTHour], f.fixhd[FH_DTMinute],
-      f.fixhd[FH_DTSecond])
+    Parameters
+    ----------
+    None
 
-print("Valid Time", f.fixhd[FH_VTYear], f.fixhd[FH_VTMonth],
-      f.fixhd[FH_VTDay], f.fixhd[FH_VTHour], f.fixhd[FH_VTMinute],
-      f.fixhd[FH_VTSecond])
+    Returns
+    ----------
+    args_parsed : argparse.Namespace
+        Argparse namespace containing the parsed command line arguments.
+    """
+    parser = argparse.ArgumentParser(description="Change the date on a UM dumpfile")
+    # Positional arguments
+    parser.add_argument('ifile', metavar="INPUT_PATH", help='Path to the input file.')
+    # Optional arguments
+    parser.add_argument('-y', dest='year', type=int, help = 'The year to be changed, must be positive')
+    parser.add_argument('-m', dest='month', type=int,help = 'The month to be changed, must be between 1-12')
+    parser.add_argument('-d', dest='day', type=int, help = 'The day to be changed must be between 1-31')
+    parser.add_argument('--validate', action='store_true', help='Validate the output fields file using mule validation.')
+    parser.add_argument('-o', '--output', dest = 'output_path', metavar="OUTPUT_PATH",
+            help='Path to the output file. If omitted, the default output file is created by appending "_perturbed" to the input path.')
+    args_parsed = parser.parse_args()
+    return args_parsed
 
-s = input('Enter year month day\n')
-s = s.split()
-year = int(s[0])
-month = int(s[1])
-day = int(s[2])
+def alter_header_date(dump_file, year, month, day):
+    # Update the initial and valid dates in the file headers
+    dump_file.fixed_length_header.t1_year = year
+    dump_file.fixed_length_header.t1_month = month
+    dump_file.fixed_length_header.t1_day = day
+      
+def alter_lookup_headers_validity_time(field, year, month,day):
 
-print("Using", year, month, day)
+    field.lbyr = year
+    field.lbmon = month
+    field.lbdat = day
 
-f.fixhd[FH_DTYear] = f.fixhd[FH_VTYear] = year
-f.fixhd[FH_DTMonth] = f.fixhd[FH_VTMonth] = month
-f.fixhd[FH_DTDay] = f.fixhd[FH_VTDay] = day
+def create_default_outname(filename, suffix="_perturbed"):
+    """
+    Create a default output filename by appending a suffix to the input filename. 
+    If an output filename already exists, a number will be appended to produce a unique output filename. 
 
-for k in range(f.fixhd[FH_LookupSize2]):
-    ilookup = f.ilookup[k]
-    lbegin = ilookup[LBEGIN] # lbegin is offset from start
-    if lbegin == -99:
-        # Ignore missing fields.
-        break
-    ilookup[LBYR] = year
-    ilookup[LBMON] = month
-    ilookup[LBDAT] = day
-    ilookup[LBFT] = 0
+    Parameters
+    ----------
+    filename: str
+         The input filename.
+    suffix: str, optional
+        The suffix to append to the filename.
 
-f.close()
+    Returns
+    ----------
+    output_filename: str 
+        The default output filename.
+    """
+    output_filename = f"{filename}{suffix}"
+    num=""
+    if os.path.exists(output_filename):
+        num = 1
+        while os.path.exists(f"{output_filename}{num}"):
+            num += 1
+    return f"{output_filename}{num}"
+
+def main():
+
+    args = parse_args()
+    ff = mule.DumpFile.from_file(args.ifile)
+
+    output_file = create_default_outname(args.ifile) if args.output_path is None else args.output_path
+
+    alter_header_date(ff, args.year, args.month, args.day)
+
+    for field in ff.fields:
+        alter_lookup_headers_validity_time(field, args.year, args.month, args.day)
+
+    dump_file.to_file(output_file)
+
+    print(f"Updated file saved as {output_file_path}")
+    f.close()
