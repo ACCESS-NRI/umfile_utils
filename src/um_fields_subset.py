d646d2818fccbb8ce5345f1bf1d8e971ab02c3be
Lindsey Oberhelman <120349369+leoberhelman@users.noreply.github.com>
2025-01-31 12:29:41 +1100

    Update um_fields_subset.py
    deleted check_packed_fields
    
    Update src/um_fields_subset.py
    
    Co-authored-by: Davide Marchegiani <davide.marchegiani@gmail.com>
    
    Update um_fields_subset.py
    
    Docstrings for the new include and exclude functions
    
    Update src/um_fields_subset.py
    
    Co-authored-by: Davide Marchegiani <davide.marchegiani@gmail.com>
    
    Update um_fields_subset.py
    
    Implementing the integration of list of integers into the args parser
    
    Update um_fields_subset.py
    
    Update um_fields_subset.py
    
    Added new function to provide a warning if a STASH code doesn't exist in the file

--- src/um_fields_subset.py
+++ src/um_fields_subset.py
@@ -11,8 +11,16 @@
 import mule
 import os
 import argparse
+import warnings
 from itertools import chain
 PROGNOSTIC_STASH_CODES = tuple(chain(range(1,999+1), range(33001,34999+1)))
+
+def convert_to_list(value: str):
+    """Convert a comma-separated string into a list of integers."""
+    try:
+        return [int(v) for v in value.split(",")]
+    except ValueError:
+        raise argparse.ArgumentTypeError("All values must be integers.")
 
 def parse_args():
     """
@@ -36,48 +44,26 @@ def parse_args():
     meg = parser.add_mutually_exclusive_group(required=True)
     meg.add_argument('-p', '--prognostic', dest='prognostic',  action='store_true',
                         help="Only include prognostic variables (sections 0, 33 and 34). Cannot be used together with --include or --exclude.")
-    meg.add_argument('--include', dest='include_list', type=str, metavar="STASH_CODE",
+    meg.add_argument('--include', dest='include_list',  type=convert_to_list, metavar="STASH_CODE",
                         help="Comma-separated list of STASH codes to include in the output file. Any STASH code present in the input file, but not contained in this STASH code list, will not be present in the output file. Cannot be used together with --prognostic or --exclude.")
-    meg.add_argument('--exclude', dest='exclude_list', type=str, metavar="STASH_CODE"
+    meg.add_argument('--exclude', dest='exclude_list',  type=convert_to_list, metavar="STASH_CODE",
                         help="Comma-separated list of STASH codes to exclude from the output file. All STASH codes present in the input file, but not contained in this STASH code list, will be present in the output file. Cannot be used together with --prognostic or --include.")
     parser.add_argument('--validate', action='store_true',
                         help='Validate the output fields file using mule validation.')
     # Parse arguments
     args_parsed = parser.parse_args()
-
-    # Convert from string to int
-    args_parsed.include_list = [int(v) for v in args_parsed.include_list.split(",")] if args_parsed.include_list else []
-    args_parsed.exclude_list = [int(x) for x in args_parsed.exclude_list.split(",")] if args_parsed.exclude_list else []
-
-
-
+    
     # Return arguments
     return args_parsed
-    
 
 
 def void_validation(*args, **kwargs):
     """
     Don't perform the validation, but print a message to inform that validation has been skipped.
     """
+
     print('Skipping mule validation. To enable the validation, run using the "--validate" option.')
 
-
-def initialize_output_file(ff):
-    """
-    Initialize the output UM file by copying the input file and preparing it for output.
-
-    Parameters
-    ----------
-    ff : mule.DumpFile
-        The input UM file object to be copied.
-    Returns
-    -------
-    mule.DumpFile
-        A new copy of the input UM file with its fields initialized to an empty list.
-    """
-    file_copy = ff.copy()
-    return file_copy
 
 def create_default_outname(filename, suffix="_subset"):
     """
@@ -96,6 +82,7 @@ def create_default_outname(filename, suffix="_subset"):
     output_filename: str
         The default output filename.
     """
+
     output_filename = f"{filename}{suffix}"
     num=""
     if os.path.exists(output_filename):
@@ -103,129 +90,104 @@ def create_default_outname(filename, suffix="_subset"):
         while os.path.exists(f"{output_filename}{num}"):
             num += 1
     return f"{output_filename}{num}"
-
-
-def filter_fields(input_file, prognostic, include_list, exclude_list):
+    
+def field_not_present_warning(fields, stash_list):
     """
-    Creates a list of fields from the input file that should be included based on user conditions.
+    Checks that the fields in the either list provided are present in the file and provides a warning if not.
+    
+    Parameters
+    __________
+
+    fields : mule.ff.FieldsFile.fields
+        All the fields from the fields file.
+
+    stash_list :  list of int
+        Either the exclude or include STASH item code list.
+    Returns
+    _______
+        None - gives a warning.
+
+    """
+
+    existing_codes = {f.lbuser4 for f in fields}
+    missing_codes = [code for code in stash_list if code not in existing_codes]
+
+    if missing_codes:
+        warnings.warn(f"Warning: STASH code(s): {missing_codes} is(are) not present in the input file.")
+
+
+def include_fields(fields, stash_list):
+    """
+    Checks if the field is in the include list and if so provides a copy of that field.
+
+    Parameters
+    __________
+    fields : mule.ff.FieldsFile.fields 
+        All the fields from the fields file.
+    stash_list : list of int
+        A list of STASH item codes to include.
+    
+    Returns
+    -------
+        A list of copies of the fields stash_list to include.
+    """
+
+    field_not_present_warning(fields, stash_list)
+    return [f.copy() for f in fields if f.lbuser4 in stash_list]
+
+def exclude_fields(fields, stash_list):
+    """
+    Checks if the field is not in the exclude list and if so provides a copy of that field.
+
+    Parameters
+    __________
+    fields : mule.ff.FieldsFile.fields 
+        All the fields from the fields file.
+    stash_list : list of int
+        A list of STASH item codes to exclude.
+    
+    Returns
+    -------
+        A list of copies of the fields stash_list to include.
+    """
+
+    field_not_present_warning(fields, stash_list)
+    return [f.copy() for f in fields if f.lbuser4 not in stash_list]
+
+def filter_fieldsfile(input_file, prognostic, include_list, exclude_list):
+    """
+    Creates a new mule fieldsfile from the input_file, by filtering its fields based on the values of prognostic, include_list and eclude_list.
 
     Parameters
     ----------
-    field : object
-        The field object to be checked.
+    input_file : mule.ff.FieldsFile
+        The mule fieldsfile to be filtered.
     prognostic : bool
         A boolean flag indicating if only prognostic fields should be included.
-    include_list : list of int
+    include_list : list of int or None
         A list of STASH item codes to include.
-    exclude_list : list of int
+    exclude_list : list of int or None
         A list of STASH item codes to exclude.
 
     Returns
     -------
-    list
-        A list of fields that need to be in the file
+    filtered_file : mule.ff.FieldsFile
+        The filtered mule fieldsfile.
     """
-    filtered_fields = []
-    for field in input_file.fields:
 
-        # Check if the field is part of the exclusion list
-        if field.stash in exclude_list:
-            continue
-            
-        # Check if the field is part of the inclusion list (if specified)
-        if include_list  and field.stash in include_list:
-            filtered_fields.append(field)
+    filtered_file = input_file.copy()
+    if prognostic:
+        include_list = PROGNOSTIC_STASH_CODES
 
-        # If no inclusion or exclusion list, include the field based on its type
-        elif prognostic and field.lbuser4 in PROG_STASH_CODES:
-            filtered_fields.append(field)
-
-        elif not prognostic and not include_list and not exclude_list:
-            filtered_fields.append(field)
-
-    return filtered_fields
-
-
-def check_packed_fields(filtered_fields):
-    """
-    Checks if packed fields in the input file require a land-sea mask and modifies
-    the include list if necessary.
-
-    Parameters
-    ----------
-    filtered_fields : list of int
-        A list of STASH item codes to include in the output. If packed fields require a
-        land-sea mask, STASH item code 30 will be added to this list.
-
-    Returns
-    -------
-    None
-        This function modifies the `include_list` in place by appending the land-sea mask
-        (STASH code 30) if required.
-    """
-    print('calling check_packed')
-    needmask, masksaved = False, False
-    print(filtered_fields)
-
-    for field in filtered_fields:
-        print(field.stash, field.lbpack, field.lblev)
-
-        needmask |= (field.lbpack == 2 and field.lblev in (1,2))
-        masksaved |= (field.stash == MASK_CODE)
-
-    if needmask and not masksaved:
-        print('adding mask')
-        filtered_fields.append(30)
-
-    else:
-        print('not adding mask')
-        print(masksaved)
-        print(needmask)
-
-    return filtered_fields
-
-def append_fields(outfile, filtered_fields):
-    """
-    Copies fields from the input UM file to the output UM file based on inclusion and exclusion criteria.
-
-    Parameters
-    ----------
-    input_file : mule.DumpFile
-        The input UM file containing the fields to be copied.
-
-    outfile : mule.DumpFile
-        The output UM file to which the selected fields will be copied.
-
-    prognostic : bool
-        If True, only prognostic fields will be copied.
-
-    include_list : list of int
-        A list of STASH item codes to include. Only these fields will be copied to the output file.
-
-    exclude_list : list of int
-        A list of STASH item codes to exclude. Fields with these item codes will not be copied to the output file.
-
-    Returns
-    -------
-    None
-        This function modifies the output_file in place and does not return any value.
-    """
-    for field in filtered_fields:
-        # Ad to the outfile fields 
-        outfile.fields.append(field.copy())
-
-
+    filtered_file.fields = include_fields(input_file.fields, include_list) if include_list is not None else exclude_fields(input_file.fields, exclude_list)
+    return filtered_file
+    
 def main():
 
     # Parse the inputs and validate that they do not xlist or vlist are given.
-    args = parse_arguments()
+    args = parse_args()
 
-    # Skip the mule validation if the "--validate" option is provided.
-    if args.validate:
-        filtered_file.validate = void_validation
-
-    ff = mule.load_umfile(args.ifile)
-
+    ff = mule.DumpFile.from_file(args.ifile)
 
     # Create the output filename.
     output_filename = create_default_outname(args.ifile) if args.output_path is None else args.output_path
@@ -233,7 +195,13 @@ def main():
     # filter the fieldsfile
     filtered_file = filter_fieldsfile(ff, args.prognostic, args.include_list, args.exclude_list)
 
-    outfile.to_file(output_filename)
-    
+    #Skip mule validation if the "--validate" option is provided
+    if not args.validate:
+        filtered_file.validate = void_validation
+
+    filtered_file.to_file(output_filename)
+
 if __name__== "__main__":
     main()
+
+
