2804952b00daa970d168a1f760e17812ebeba4fa
Lindsey Oberhelman <120349369+leoberhelman@users.noreply.github.com>
2025-02-17 21:39:04 +1100

    Update change_dump_date.py

--- src/change_dump_date.py
+++ src/change_dump_date.py
@@ -1,14 +1,6 @@
-#!/usr/bin/env python
-# Change the initial and valid date of a UM dump file
-
-# Change both the file header and the date header of each record.
-# The latter may not be strictly necessary but it makes looking at file with
-# xconv less confusing.
-
-import getopt, sys
-import argparse
+import os
 import mule
-
+import argparse
 
 def parse_args():
     """
@@ -23,32 +15,70 @@ def parse_args():
     args_parsed : argparse.Namespace
         Argparse namespace containing the parsed command line arguments.
     """
-    parser = argparse.ArgumentParser(description="Change the date on a UM dumpfile")
-    # Positional arguments
+    parser = argparse.ArgumentParser(description="Modify UM dump file timestamps.")
     parser.add_argument('ifile', metavar="INPUT_PATH", help='Path to the input file.')
-    # Optional arguments
-    parser.add_argument('-y', dest='year', type=int, help = 'The year to be changed, must be positive')
-    parser.add_argument('-m', dest='month', type=int,help = 'The month to be changed, must be between 1-12')
-    parser.add_argument('-d', dest='day', type=int, help = 'The day to be changed must be between 1-31')
-    parser.add_argument('--validate', action='store_true', help='Validate the output fields file using mule validation.')
-    parser.add_argument('-o', '--output', dest = 'output_path', metavar="OUTPUT_PATH",
-            help='Path to the output file. If omitted, the default output file is created by appending "_perturbed" to the input path.')
-    args_parsed = parser.parse_args()
-    return args_parsed
+    parser.add_argument('-y', '--year', type=int, help='New year value.', required=True)
+    parser.add_argument('-m', '--month', type=int, help='New month value (1-12).', required=True)
+    parser.add_argument('-d', '--day', type=int, help='New day value (1-31).', required=True)
+    parser.add_argument('-o', '--output', dest='output_path', metavar="OUTPUT_PATH",
+                        help='Path to the output file. If omitted, the default output file is created by appending "_perturbed" to the input path.')
+    parser.add_argument('--validate', action='store_true',
+        help='Validate the output fields file using mule validation.')
+    return parser.parse_args()
 
-def alter_header_date(dump_file, year, month, day):
-    # Update the initial and valid dates in the file headers
-    dump_file.fixed_length_header.t1_year = year
-    dump_file.fixed_length_header.t1_month = month
-    dump_file.fixed_length_header.t1_day = day
-      
-def alter_lookup_headers_validity_time(field, year, month,day):
+def change_fileheader_date(ff, new_year, new_month, new_day):
+    """
+    Update the initial and valid date in the fixed-length header of a UM fields file.
 
-    field.lbyr = year
-    field.lbmon = month
-    field.lbdat = day
+    Parameters
+    ----------
+    ff : mule.FieldsFile
+        The UM fields file object whose header will be updated.
+    new_year : int
+        The new year to set in the file header.
+    new_month : int
+        The new month to set in the file header.
+    new_day : int
+        The new day to set in the file header.
 
-def create_default_outname(filename, suffix="_perturbed"):
+    Returns
+    -------
+    None
+    """
+    ff.fixed_length_header.t1_year = new_year
+    ff.fixed_length_header.t1_month = new_month
+    ff.fixed_length_header.t1_day = new_day
+
+    ff.fixed_length_header.v1_year = new_year
+    ff.fixed_length_header.v1_month = new_month
+    ff.fixed_length_header.v1_day = new_day
+
+def change_fieldheader_date(ff, new_year, new_month, new_day):
+    """
+    Update the header date of each field in the  UM fields file.
+
+    Parameters
+    ----------
+    ff : mule.FieldsFile
+        The UM fields file object whose header will be updated.
+    new_year : int
+        The new year to set in the file header.
+    new_month : int
+        The new month to set in the file header.
+    new_day : int
+        The new day to set in the file header.
+
+    Returns
+    -------
+    None
+    """
+
+    for field in ff.fields:
+        field.lbyr = new_year
+        field.lbmon = new_month
+        field.lbdat = new_day
+
+def create_default_outname(filename, suffix="_date_changed"):
     """
     Create a default output filename by appending a suffix to the input filename. 
     If an output filename already exists, a number will be appended to produce a unique output filename. 
@@ -73,19 +103,36 @@ def create_default_outname(filename, suffix="_perturbed"):
             num += 1
     return f"{output_filename}{num}"
 
-def main():
+def void_validation(*args, **kwargs):
+    """
+    Don't perform the validation, but print a message to inform that validation has been skipped.
+    """
+    print('Skipping mule validation. To enable the validation, run using the "--validate" option.')
 
+def main():
+    """
+    Main function to load, modify, and save the UM dump file.
+    """
     args = parse_args()
-    ff = mule.DumpFile.from_file(args.ifile)
+
+    ff = mule.FieldsFile.from_file(args.ifile)
 
     output_file = create_default_outname(args.ifile) if args.output_path is None else args.output_path
+    print(ff.fixed_length_header.t1_year)
+    # Skip mule validation if the "--validate" option is provided
 
-    alter_header_date(ff, args.year, args.month, args.day)
+    if not args.validate:
+        ff.validate = void_validation
 
-    for field in ff.fields:
-        alter_lookup_headers_validity_time(field, args.year, args.month, args.day)
+    change_fileheader_date(ff, args.year, args.month, args.day)
+    change_fieldheader_date(ff, args.year, args.month, args.day)
 
-    dump_file.to_file(output_file)
+    print(ff.fixed_length_header.t1_year)
+    # Create the output filename
 
-    print(f"Updated file saved as {output_file_path}")
-    f.close()
+    ff.to_file(output_file)
+
+
+if __name__== "__main__":
+
+    main()
